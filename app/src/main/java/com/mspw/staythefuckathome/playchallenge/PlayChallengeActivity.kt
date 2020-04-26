255ba05fbe1d370bb14d61fb48742d39b885816b
HeoHwan <mnnmnnm7@gmail.com>
2020-04-26 16:41:35 +0900

    Video parameter 적용

--- app/src/main/java/com/mspw/staythefuckathome/playchallenge/PlayChallengeActivity.kt
+++ app/src/main/java/com/mspw/staythefuckathome/playchallenge/PlayChallengeActivity.kt
@@ -1,8 +1,12 @@
 package com.mspw.staythefuckathome.playchallenge
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
@@ -22,7 +26,7 @@ class PlayChallengeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_challenge)
-        video = intent.getParcelableExtra("video")
+        setupUi()
         setupListener()
         initializePlayer()
     }
@@ -37,13 +41,30 @@ class PlayChallengeActivity : AppCompatActivity() {
         player?.playWhenReady = false
     }
 
+    private fun setupUi() {
+        playChallengeUsername.text = video?.user?.name
+        try {
+            video = intent.getParcelableExtra("video")
+            val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
+            Glide.with(playChallengeUserProfile.context)
+                .load(video?.user?.image)
+                .transition(DrawableTransitionOptions.with(factory))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .circleCrop()
+                .placeholder(R.drawable.circle_dark_white)
+                .error(R.drawable.circle_dark_white)
+                .into(playChallengeUserProfile)
+        } catch (exception: Exception) {
+        }
+    }
+
     private fun setupListener() {
         playChallengeClose.setOnClickListener { onBackPressed() }
     }
 
     private fun initializePlayer() {
         try {
-            val uri = Uri.parse("https://d23r2dimy698xl.cloudfront.net/media/test.mp4")
+            val uri = Uri.parse(video?.url)
             val applicationId = applicationInfo.labelRes
             val applicationName = getString(applicationId)
             val dataSourceFactory = DefaultDataSourceFactory(
@@ -58,7 +79,6 @@ class PlayChallengeActivity : AppCompatActivity() {
                     addListener(object : Player.EventListener {
                         override fun onPlayerError(error: ExoPlaybackException?) {
                             super.onPlayerError(error)
-//                            handleResourceNotFound()
                         }
 
                         override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
@@ -69,12 +89,17 @@ class PlayChallengeActivity : AppCompatActivity() {
                         }
                     })
                 }
-
             playerView.player = player
             player?.prepare(mediaSource)
         } catch (exception: NullPointerException) {
 //            handleResourceNotFound()
         }
+    }
+
+    companion object {
+        private val TAG = PlayChallengeActivity::class.java.simpleName
+        private const val SPECIES_RATIO = 9
+        private const val TRANSVERSE_RATIO = 16
     }
 
 
