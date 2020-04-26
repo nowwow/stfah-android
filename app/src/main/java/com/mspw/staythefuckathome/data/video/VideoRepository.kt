7ee147df258915589e186f0a821196a351414205
nonop1507 <nono1507@naver.com>
2020-04-26 18:43:37 +0900

    My page api

--- app/src/main/java/com/mspw/staythefuckathome/data/video/VideoRepository.kt
+++ app/src/main/java/com/mspw/staythefuckathome/data/video/VideoRepository.kt
@@ -3,6 +3,7 @@ package com.mspw.staythefuckathome.data.video
 import com.mspw.staythefuckathome.data.ListResponse
 import io.reactivex.Flowable
 import io.reactivex.Single
+import retrofit2.Call
 
 class VideoRepository(
     private val videoService: VideoService
@@ -14,6 +15,10 @@ class VideoRepository(
 
     fun findOneById(id: Long): Single<Video> {
         return Single.just(Video())
+    }
+
+    fun findAllVideoByCreatorId(creator:Long): Call<ListResponse<Video>> {
+        return videoService.findAllVideoByCreatorId(creator)
     }
 
 }
--- app/src/main/java/com/mspw/staythefuckathome/data/video/VideoService.kt
+++ app/src/main/java/com/mspw/staythefuckathome/data/video/VideoService.kt
@@ -2,6 +2,7 @@ package com.mspw.staythefuckathome.data.video
 
 import com.mspw.staythefuckathome.data.ListResponse
 import io.reactivex.Flowable
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,5 +12,10 @@ interface VideoService {
     fun findAllByChallengeId(
         @Query("challenge_id") challengeId: Long
     ): Flowable<ListResponse<Video>>
+
+    @GET("videos/")
+    fun findAllVideoByCreatorId(
+        @Query("creator") creatorId: Long
+    ): Call<ListResponse<Video>>
 
 }
