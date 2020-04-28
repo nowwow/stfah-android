e881f9f9819e16c3c4af74a38b50a5d99a59d91b
nonop1507 <nono1507@naver.com>
2020-04-28 12:09:06 +0900

    Change base url

--- app/src/main/java/com/mspw/staythefuckathome/AppContainer.kt
+++ app/src/main/java/com/mspw/staythefuckathome/AppContainer.kt
@@ -38,7 +38,7 @@ class AppContainer {
     val videoRepository = VideoRepository(videoService)
 
     companion object {
-        private const val BASE_URL = "https://api.staythefxxxathome.com/"
+        private const val BASE_URL = "https://api.staythefxxxathome.com/v1/"
         private const val TIME_OUT_SECONDS = 9000L
     }
 
